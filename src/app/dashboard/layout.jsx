573586a0f7f862e270f76dcc082a10d50b1558bd
Mehrab <mmehrab.pk@gmail.com>
2026-07-01 17:55:42 +0330

    code update

---
+++ src/app/dashboard/layout.jsx
@@ -0,0 +1,75 @@
+"use client";
+
+import AppSidebar from "@/components/sidebar/AppSidebar";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+import { Separator } from "@/components/ui/separator";
+
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+export default function DashboardLayout({ children }) {
+  return (
+    <SidebarProvider>
+      
+      {/* Sidebar */}
+      <AppSidebar />
+
+      {/* Main Content */}
+      <SidebarInset>
+        
+        {/* Header */}
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+          <div className="flex items-center gap-2 px-4">
+
+            <SidebarTrigger className="-ml-1" />
+
+            <Separator
+              orientation="vertical"
+              className="mr-2 h-4"
+            />
+
+            <Breadcrumb>
+              <BreadcrumbList>
+
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Dashboard
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+
+                <BreadcrumbSeparator className="hidden md:block" />
+
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Overview</BreadcrumbPage>
+                </BreadcrumbItem>
+
+              </BreadcrumbList>
+            </Breadcrumb>
+
+          </div>
+        </header>
+
+        {/* Page Content */}
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+
+          {children}
+
+        </div>
+
+      </SidebarInset>
+
+    </SidebarProvider>
+  );
+}
