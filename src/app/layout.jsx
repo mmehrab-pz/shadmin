e41ce0f67c79fc28991162c093f831d8626f4401
Mehrab <mmehrab.pk@gmail.com>
2026-07-01 16:38:52 +0330

    font fixed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,16 +1,21 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import ThemeProvider from "@/providers/ThemeProvider";
 import "./globals.css";
 import NextThemeProvider from "@/providers/NextThemeProvider";
 
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
 });
 
 export const metadata = {
@@ -22,7 +27,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <NextThemeProvider>
